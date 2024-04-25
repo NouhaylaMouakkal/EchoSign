77a9a01c4d8c62a81de7936254fa5e27e39f3427
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-04-25 09:21:28 +0100

    add tailwind

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignTotextComponent } from './sign-totext/sign-totext.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign_traduction', component: SignTotextComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
