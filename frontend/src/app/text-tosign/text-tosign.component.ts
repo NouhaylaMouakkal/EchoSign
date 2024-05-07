54d8650c51e2b35afd66243bb4aa0c7709ba7eb8
NouhaylaMouakkal <nouhaylamouakkalwa19@gmail.com>
2024-05-07 15:38:26 +0100

    fix display

--- frontend/src/app/text-tosign/text-tosign.component.ts
+++ frontend/src/app/text-tosign/text-tosign.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-text-tosign',
@@ -9,27 +8,31 @@ import { HttpClient } from '@angular/common/http';
 export class TextTosignComponent {
   messageContent: string = '';
   messages: string[] = [];
-  videoPath: string = '';
 
-  constructor(private http: HttpClient) {}
-
-  generateVideo() {
+  sendMessage() {
     if (this.messageContent.trim() !== '') {
-      this.http.post<any>('/generate-video', {
-        texte: this.messageContent,
-        target_width: 600, 
-        target_height: 500, 
-        delay_between_letters: 10 
-      }).subscribe(
-        (data) => {
-          this.videoPath = data.video_path;
-        },
-        (error) => {
-          console.error('Erreur lors de la génération de la vidéo :', error);
-        }
-      );
       this.messages.push(this.messageContent);
       this.messageContent = '';
     }
   }
 }
+
+// generateVideo() {
+  //   if (this.messageContent.trim() !== '') {
+  //     this.http.post<any>('/generate-video', {
+  //       texte: this.messageContent,
+  //       target_width: 600, 
+  //       target_height: 500, 
+  //       delay_between_letters: 10 
+  //     }).subscribe(
+  //       (data) => {
+  //         this.videoPath = data.video_path;
+  //       },
+  //       (error) => {
+  //         console.error('Erreur lors de la génération de la vidéo :', error);
+  //       }
+  //     );
+  //     this.messages.push(this.messageContent);
+  //     this.messageContent = '';
+  //   }
+  // }
