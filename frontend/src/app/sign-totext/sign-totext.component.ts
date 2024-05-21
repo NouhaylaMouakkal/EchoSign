a3b8d486079768dbbf974c8dad1b8b648a97f2ea
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-05-21 16:24:04 +0100

    fix errors sign2text 1

--- frontend/src/app/sign-totext/sign-totext.component.ts
+++ frontend/src/app/sign-totext/sign-totext.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-sign-totext',
@@ -10,11 +11,12 @@ export class SignTotextComponent implements OnInit, OnDestroy {
   webcamVideo!: ElementRef<HTMLVideoElement>;
   stream!: MediaStream | null;
   isWebcamStarted = false;
-
+  private waveSurfer!: WaveSurfer;
   constructor() { }
 
   ngOnInit() {
     this.initWebcam();
+    this.initializeWaveSurfer();
   }
 
   ngOnDestroy() {
@@ -50,6 +52,30 @@ export class SignTotextComponent implements OnInit, OnDestroy {
       this.stream.getTracks().forEach(track => track.stop());
       this.stream = null!;
       this.isWebcamStarted = false;
+    }
+  }
+
+  private initializeWaveSurfer(): void {
+    this.waveSurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'rgba(255, 255, 255, 0.9)', 
+      progressColor: 'rgb(25, 255, 255)',
+      cursorColor: 'rgba(255, 255, 255, 0.7)',
+      barWidth: 3,
+      barRadius: 5,
+      cursorWidth: 1,
+      height: 40,
+      barGap: 2
+    });
+
+    this.waveSurfer.load('assets/test.mp3');
+  }
+
+  public togglePlayPause(): void {
+    if (this.waveSurfer.isPlaying()) {
+      this.waveSurfer.pause();
+    } else {
+      this.waveSurfer.play();
     }
   }
 }
