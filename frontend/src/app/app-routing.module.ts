bca6f459743bc2890b44ec2a64b7d1d664a2ca7c
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-12-21 19:15:08 +0100

    CI/CD Frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,18 +4,22 @@ import { SignTotextComponent } from './sign-totext/sign-totext.component';
 import { TextTosignComponent } from './text-tosign/text-tosign.component';
 import { HomeComponent } from './home/home.component';
 
-
 const routes: Routes = [
   { path: 'sign_traduction', component: SignTotextComponent },
   { path: 'text_traduction', component: TextTosignComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' } // Handle undefined routes
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled',
-  anchorScrolling: 'enabled',
-  scrollOffset: [0, 64]})],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 64]
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
